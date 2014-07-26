401085a7c366bb8cab8d1086abce529b048a7d9b
brero12 <brero12@gmail.com>
2014-07-25 22:08:34 -0500

    rest file
    .

---
+++ menu.php
@@ -0,0 +1,100 @@
+<ul class="sidebar-menu">
+	<li class="active">
+		<a href="index.php">
+			<i class="fa fa-dashboard"></i> <span>Principal</span>
+		</a>
+	</li>
+	<li>
+		<a href="JavaScript:cargarMapas(this)">
+			<i class="fa fa-th"></i> <span>Mapas</span> <small class="badge pull-right bg-green">new</small>
+		</a>
+	</li>
+	
+	
+	
+	
+	<li class="treeview">
+		<a href="#">
+			<i class="fa fa-table"></i> <span>Administraci&oacute;n</span>
+			<i class="fa fa-angle-left pull-right"></i>
+		</a>
+		<ul class="treeview-menu">
+			<li><a href="pages/tables/simple.html"><i class="fa fa-angle-double-right"></i> Crear Usuario</a></li>
+			<li><a href="pages/tables/data.html"><i class="fa fa-angle-double-right"></i> Adicionar Familia</a></li>
+			<li><a href="pages/tables/data.html"><i class="fa fa-angle-double-right"></i> Modificar Usuario</a></li>
+		</ul>
+	</li>
+	
+	<li>
+		<a href="JavaScript:cargarCalendario(this)">
+			<i class="fa fa-calendar"></i> <span>Calendario</span>
+			<small class="badge pull-right bg-red">3</small>
+		</a>
+	</li>
+	
+	<li class="treeview">
+		<a href="#">
+			<i class="fa fa-bar-chart-o"></i>
+			<span>Reportes</span>
+			<i class="fa fa-angle-left pull-right"></i>
+		</a>
+		<ul class="treeview-menu">
+			<li><a href="JavaScript:cargarReporte1(this)"><i class="fa fa-angle-double-right"></i> Reporte 1</a></li>
+			<li><a href="JavaScript:cargarReporte1(this)"><i class="fa fa-angle-double-right"></i> Reporte 2 </a></li>
+			<li><a href="JavaScript:cargarReporte1(this)"><i class="fa fa-angle-double-right"></i> Reporte 3 </a></li>
+		</ul>
+	</li>
+	
+	<!-- 
+		<li class="treeview">
+		<a href="#">
+		<i class="fa fa-laptop"></i>
+		<span>UI Elements</span>
+		<i class="fa fa-angle-left pull-right"></i>
+		</a>
+		<ul class="treeview-menu">
+		<li><a href="pages/UI/general.html"><i class="fa fa-angle-double-right"></i> General</a></li>
+		<li><a href="pages/UI/icons.html"><i class="fa fa-angle-double-right"></i> Icons</a></li>
+		<li><a href="pages/UI/buttons.html"><i class="fa fa-angle-double-right"></i> Buttons</a></li>
+		<li><a href="pages/UI/sliders.html"><i class="fa fa-angle-double-right"></i> Sliders</a></li>
+		<li><a href="pages/UI/timeline.html"><i class="fa fa-angle-double-right"></i> Timeline</a></li>
+		</ul>
+		</li>
+		
+		<li class="treeview">
+		<a href="#">
+		<i class="fa fa-edit"></i> <span>Forms</span>
+		<i class="fa fa-angle-left pull-right"></i>
+		</a>
+		<ul class="treeview-menu">
+		<li><a href="pages/forms/general.html"><i class="fa fa-angle-double-right"></i> General Elements</a></li>
+		<li><a href="pages/forms/advanced.html"><i class="fa fa-angle-double-right"></i> Advanced Elements</a></li>
+		<li><a href="pages/forms/editors.html"><i class="fa fa-angle-double-right"></i> Editors</a></li>
+		</ul>
+		</li>
+		
+		
+		<li>
+		<a href="pages/mailbox.html">
+		<i class="fa fa-envelope"></i> <span>Mailbox</span>
+		<small class="badge pull-right bg-yellow">12</small>
+		</a>
+		</li>
+		
+		<li class="treeview">
+		<a href="#">
+		<i class="fa fa-folder"></i> <span>Examples</span>
+		<i class="fa fa-angle-left pull-right"></i>
+		</a>
+		<ul class="treeview-menu">
+		<li><a href="pages/examples/invoice.html"><i class="fa fa-angle-double-right"></i> Invoice</a></li>
+		<li><a href="pages/examples/login.html"><i class="fa fa-angle-double-right"></i> Login</a></li>
+		<li><a href="pages/examples/register.html"><i class="fa fa-angle-double-right"></i> Register</a></li>
+		<li><a href="pages/examples/lockscreen.html"><i class="fa fa-angle-double-right"></i> Lockscreen</a></li>
+		<li><a href="pages/examples/404.html"><i class="fa fa-angle-double-right"></i> 404 Error</a></li>
+		<li><a href="pages/examples/500.html"><i class="fa fa-angle-double-right"></i> 500 Error</a></li>
+		<li><a href="pages/examples/blank.html"><i class="fa fa-angle-double-right"></i> Blank Page</a></li>
+		</ul>
+		</li>
+	-->
+</ul>
---
+++ view/cargarMapa.js
@@ -0,0 +1,9 @@
+	include("js/jquery.maphilight.js");
+
+        $('.map').maphilight();
+        $('#squidheadlink').mouseover(function(e) {
+            $('#squidhead').mouseover();
+        }).mouseout(function(e) {
+            $('#squidhead').mouseout();
+        }).click(function(e) { e.preventDefault(); });
+    
